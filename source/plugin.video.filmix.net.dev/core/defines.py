fb79f2d6989e637c696af6beaa19ab32d2e3e506
rvlad1987 <17516082+rvlad1987@users.noreply.github.com>
2018-02-23 15:50:27 +0500

    filmix 0.3.3
    Добавлена возможность вручную прописывать в настройках домен сайта

--- source/plugin.video.filmix.net.dev/core/defines.py
+++ source/plugin.video.filmix.net.dev/core/defines.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
+import xbmcup.app
 
-SITE_DOMAIN = 'filmix.cc'
-SITE_URL = 'http://'+SITE_DOMAIN
+s_https = ''
+if xbmcup.app.setting['use_https'] == 'true': s_https = 's'
+
+SITE_DOMAIN = xbmcup.app.setting['site_domain']
+SITE_URL = 'http' + s_https + '://' + SITE_DOMAIN
 COLLECTIONS_URL = SITE_URL + '/playlists/my'
 PLUGIN_ID = 'plugin.video.filmix.net.dev'
 CACHE_DATABASE = 'filmix.cache.db'
